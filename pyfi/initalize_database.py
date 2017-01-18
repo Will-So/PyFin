138991871c8430bd14edf736d53778574b76e2ea
Will-So <wil.sorenson@gmail.com>
2017-01-18 09:09:45 -0500

    re-introduced cursor to deal with testing cases; added create-cookies

--- pyfi/initalize_database.py
+++ pyfi/initalize_database.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect('accounts.db')
 cursor = conn.cursor()
 
-def getTables():
+def getTables(cursor):
    """
    Get a list of all tables
    """
@@ -12,30 +12,41 @@ def getTables():
    names = [row[0] for row in cursor.fetchall()]
    return names
 
-def is_table(nameTbl):
+def is_table(cursor, nameTbl):
    """
    Determine if a table exists
    """
-   return (nameTbl in getTables())
+   return (nameTbl in getTables(cursor))
 
 
-def create_tables():
+def create_asset_tables(cursor):
     '''
     Creates tables if they don't already exist
     :return:
     '''
-    if not is_table('mint_accounts'):
+    if not is_table(cursor, 'mint_accounts'):
         cursor.execute('''CREATE TABLE mint_accounts
                  (date text, account text, change real, balance real)''')
 
-    if not is_table('stock_accounts'):
+    if not is_table(cursor, 'stock_accounts'):
         cursor.execute('''CREATE TABLE stock_accounts
         (account text, balance real, change real)''')
 
-    if not is_table('p2p_accounts'):
+    if not is_table(cursor, 'p2p_accounts'):
         cursor.execute('''CREATE TABLE p2p_accounts
         (account text, balance real, change real)''')
 
 
-if __name__ == '__main__':
-    create_tables()
+def create_cookies_table(cursor):
+    '''
+
+    :param cursor:
+    :return:
+    '''
+    if not is_table(cursor, 'cookies'):
+        cursor.execute("""CREATE TABLE cookies
+        (name text, cookie text)""")
+
+if __name__ == '__main___':
+    create_cookies_table(cursor)
+    create_asset_tables(cursor)
