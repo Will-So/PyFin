bfb42e529fd4bcdf195dc31e6dee6e6f58e53da7
Will-So <wil.sorenson@gmail.com>
2017-01-24 13:46:38 -0500

    rewrote practically the entire script; database writes working but the catch to prevent multiple writes a day isn't

--- pyfi/pull_data/mint.py
+++ pyfi/pull_data/mint.py
@@ -15,15 +15,36 @@ import time
 import arrow
 import sqlite3
 
-from pyfi.config import mint_login, exclude
+from pyfi.config import mint_credentials, exclude, refresh, logger, GEN_TEST
 from pyfi.pull_data.get_mint_cookies import get_session_cookies
+from pyfi.utilities import generate_test_data
 
 database = sqlite3.connect('../accounts.db')
 cursor = database.cursor()
 
-mint = mintapi.Mint(mint_login[0], mint_login[1], '7912A452BCC946A7A2079DF3CAC6FE7F',
+mint = mintapi.Mint(mint_credentials[0], mint_credentials[1], '7912A452BCC946A7A2079DF3CAC6FE7F',
                     '234e7ea456584249ada51a72756e4916')
 
+
+def execute_pull():
+    """
+    Main execution logic for pulling data from mint
+
+    :return:
+    """
+    mint = mint_login(cursor)
+    logger.info("Login successful")
+    if refresh:
+        logger.info("Started refreshing")
+        refresh_accounts()
+        logger.info("Refreshed accounts")
+
+    net_worth, account_details = get_account_details(mint)
+    if GEN_TEST:
+        generate_test_data(account_details, 'account_details')
+    logger.info("Successfully retrieved {} accounts".format(len(account_details)))
+    write_accounts(account_details=account_details, exclude=exclude, cursor=cursor)
+    logger.info("Finished pulling data from mint")
 
 def mint_login(cursor):
     """
@@ -34,20 +55,24 @@ def mint_login(cursor):
     :return:
     """
     # Order by name to insure that ius comes before thx.
-    cookies = cursor.execute('''SELECT * from cookies
+    cookies = cursor.execute('''SELECT * from credentials
                                 WHERE name in ('mint_ius', 'mint_thx')
                                 ORDER BY name ASC''').fetchall()
 
     if len(cookies) == 2:
-        ius, thx = cookies[0], cookies[1]
+        # query returns a list of tuples, always want the values ([1] element)
+        ius, thx = cookies[0][1], cookies[1][1]
     else:
         ## Get the cookies by opening up selenium. If 2FA is enabled,
         ##  the user will be required to enter it in.
-        ius, thx = get_session_cookies(mint_login[0], mint_login[1])
-        cursor.execute("""INSERT INTO cookies values ('mint_ius', ?)""", ius)
-        cursor.execute("""INSERT INTO cookies values ('mint_thx', ?)""", thx)
+        values = get_session_cookies(mint_credentials[0], mint_credentials[1])
+        ius, thx = values['ius_session'], values['thx_guid']
+        cursor.execute("""INSERT INTO credentials values ('mint_ius', ?)""", (ius, ))
+        cursor.execute("""INSERT INTO credentials values ('mint_thx', ?)""", (thx, ))
+        database.commit()
+        database.close()
 
-    return mintapi.Mint(mint_login[0], mint_login[1], ius, thx)
+    return mintapi.Mint(mint_credentials[0], mint_credentials[1], ius, thx)
 
 
 # TODO: Not all of the accounts that are listed here are relevant. Deal with this in future
@@ -83,25 +108,51 @@ def write_accounts(account_details, exclude, cursor):
     :param account_details: Mint account details, containing all information in a dict
     :param exclude: accounts that should be ignore
     :param cursor: cursor to handle writing to database
+
     :return: Nothing; writes to database
     """
-    today = arrow.now().date()
+    today = str(arrow.now().date()) # Don't want to deal with datetime representation
     for account in account_details:
-        if (account['fiLoginDisplayName'] not in exclude and
+        account_name = account['accountName']
+        if (account_name not in exclude and
                     account['accountType'] != 'investment'):
-            account_name = account['fiLoginDisplayName']
-            previous_balance = cursor.execute("""SELECT balance FROM mint_accounts
-                                       WHERE account = {account} ORDER BY date DESC
-                                       LIMIT 1""".format(account=account['fiLoginDisplayName'])).fetch()
+
+            ## Both accountName and accountId are required. accountName is the user readable
+            ## but not unique
+            account_id = account['accountId']
+            account_type = account['accountType']
+            print(account_id)
+            previous_info = cursor.execute("""SELECT date, balance FROM mint_accounts
+                                       WHERE account_id = "{account_id}" ORDER BY date DESC
+                                       LIMIT 1""".format(account_id=account_id)).fetchone()
 
             # Case when there is a new account that the database hasn't seen before
-            if not previous_balance:
+            if not previous_info:
                 previous_balance = 0
+            else:
+                previous_balance = previous_info[1]
 
+            # Calculate variables to be retrieved
             balance = account['currentBalance']
+            if account_type == 'credit':
+                balance = -balance
             change = balance - previous_balance
+            print(previous_info, today)
 
-            cursor.execute("""INSERT INTO mint_accounts VALUES
-                            ({today}, {account}, {change}, {balance})""".format(
-                today=today, account=account_name, change=change, balance=balance
-            ))
+            if previous_info is None or previous_info[0] != today:
+                cursor.execute("""INSERT INTO mint_accounts VALUES
+                               ({today}, "{account}", {account_id}, {change}, {balance},
+                               "{account_type}")""".format(
+                                today=today, account=account_name, change=change,
+                                balance=balance, account_id=account_id,
+                                account_type=account_type))
+
+                logger.info("Wrote {}, {} to database".format(account_name, account_id))
+            else:
+                logger.warn("Accounts have already been updated today; skipping now.")
+
+    database.commit()
+    database.close()
+
+if __name__ == '__main__':
+    execute_pull()
