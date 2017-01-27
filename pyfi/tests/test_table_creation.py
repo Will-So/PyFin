95620b776cc7985cbab7f43f291aad870022ea4e
Will-So <wil.sorenson@gmail.com>
2017-01-26 19:01:25 -0500

    finish these tests

--- pyfi/tests/test_table_creation.py
+++ pyfi/tests/test_table_creation.py
@@ -1,12 +1,16 @@
-from ..initalize_database import create_asset_tables, get_tables
+from ..initalize_database import (create_asset_tables, get_tables, create_net_worth_table,
+                                  create_credentials_table)
 
 import sqlite3
 
 
-def test_table_creation():
+def test_asset_creation():
     conn = sqlite3.connect(':memory:')
     cursor = conn.cursor()
     assert len(get_tables(cursor)) == 0
     create_asset_tables(cursor)
     assert len(get_tables(cursor)) == 3
+    create_credentials_table(cursor)
+    create_net_worth_table(cursor)
+
 
